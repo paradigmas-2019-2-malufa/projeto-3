ef263ae9168baa80c2f1f383224af20c69140a55
Lucas <l.lopes.fga@gmail.com>
2019-11-24 22:40:16 -0200

    Prepara main teste
    Co-authored-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- CerradoPizzas/src/main/Main.java
+++ CerradoPizzas/src/main/Main.java
@@ -1,5 +1,3 @@
-
-
 package main;
 
 import model.Pedido;
@@ -9,14 +7,25 @@ import util.Gerador;
 public class Main {
 
     public static void main(String[] args) {
+        final String prompt = "bash";
+        final String op = "-c";
+        String json;
+        String commmandSend;
+        final String url = "http://localhost:3000/post/";
         ComunicadorAPI.enviarDados();
 //        ComunicadorAPI.enviarDados("echo '"+Gerador.novoPedidoJSON()+"'");
         for(int i = 0; i < 10; i++)
-			try {
-				ComunicadorAPI.enviarDados("echo '"+Gerador.novoPedidoJSON()+"'");
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            try {
+                json = Gerador.novoPedidoJSON();
+                commmandSend = "curl --header \"Content-Type: application/json\" "
+                + "  --request POST "
+                + "  --data '" + json + "' ";
+                commmandSend += url;
+                ComunicadorAPI.enviarDados(prompt, op, commmandSend);
+                ComunicadorAPI.enviarDados("echo '"+Gerador.novoPedidoJSON()+"'");
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                e.printStackTrace();
+                }
     }
 }
