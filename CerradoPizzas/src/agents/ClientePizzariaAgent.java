87f12112d65e13b659d77a4d7118f7ac6ef2e71b
FabianaRibas <fabianalv.ribas@gmail.com>
2019-11-24 22:05:05 -0300

    Add seller e modificando buyer
    Signed-off-by: FabianaRibas <fabianalv.ribas@gmail.com>
    Co-authored-by: Marcelo Araujo <santos46ma@gmail.com>

--- CerradoPizzas/src/agents/ClientePizzariaAgent.java
+++
@@ -1,7 +0,0 @@
-
-
-package agents;
-
-public class ClientePizzariaAgent {
-
-}
--- CerradoPizzas/src/agents/PizzariaAgent.java
+++ CerradoPizzas/src/agents/PizzariaAgent.java
@@ -1,191 +1,170 @@
 package agents;
 import jade.core.Agent;
-import jade.core.AID;
 import jade.core.behaviours.*;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDeion;
+import jade.domain.FIPAAgentManagement.ServiceDeion;
+import java.util.*;
 
 public class PizzariaAgent extends Agent {
-
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	// The title of the book to buy
-	private String targetBookTitle;
-	// The list of known seller agents
-	private AID[] sellerAgents;
+	// The catalogue of books for sale (maps the title of a book to its price)
+	private Hashtable<String,Integer> catalogue;
+	// The GUI by means of which the user can add books in the catalogue
+	private BookSellerGui myGui;
 
 	// Put agent initializations here
 	protected void setup() {
-		// Printout a welcome message
-		System.out.println("Hallo! Buyer-agent "+getAID().getName()+" is ready.");
+		// Create the catalogue
+		catalogue = new Hashtable<String,Integer>();
 
-		// Get the title of the book to buy as a start-up argument
-		Object[] args = getArguments();
-		if (args != null && args.length > 0) {
-			targetBookTitle = (String) args[0];
-			System.out.println("Target book is "+targetBookTitle);
+		// Create and show the GUI 
+		myGui = new BookSellerGui(this);
+		myGui.showGui();
 
-			// Add a TickerBehaviour that schedules a request to seller agents every 10 seconds
-			addBehaviour(new TickerBehaviour(this, 10000) {
-
-				private static final long serialVersionUID = 1L;
-
-				protected void onTick() {
-					System.out.println("Trying to buy "+targetBookTitle);
-					// Update the list of seller agents
-					DFAgentDescription template = new DFAgentDescription();
-					ServiceDescription sd = new ServiceDescription();
-					sd.setType("book-selling");
-					template.addServices(sd);
-					try {
-						DFAgentDescription[] result = DFService.search(myAgent, template); 
-						System.out.println("Found the following seller agents:");
-						sellerAgents = new AID[result.length];
-						for (int i = 0; i < result.length; ++i) {
-							sellerAgents[i] = result[i].getName();
-							System.out.println(sellerAgents[i].getName());
-						}
-					}
-					catch (FIPAException fe) {
-						fe.printStackTrace();
-					}
-
-					// Perform the request
-					myAgent.addBehaviour(new RequestPerformer());
-				}
-			} );
+		// Register the book-selling service in the yellow pages
+		DFAgentDeion dfd = new DFAgentDeion();
+		dfd.setName(getAID());
+		ServiceDeion sd = new ServiceDeion();
+		sd.setType("book-selling");
+		sd.setName("JADE-book-trading");
+		dfd.addServices(sd);	
+		try {
+			DFService.register(this, dfd);
 		}
-		else {
-			// Make the agent terminate
-			System.out.println("No target book title specified");
-			doDelete();
+		catch (FIPAException fe) {
+			fe.printStackTrace();
 		}
+
+		// Add the behaviour serving queries from buyer agents
+		// Servidor de Requisi��es de Ofertas
+		addBehaviour(new OfferRequestsServer());
+
+		// Add the behaviour serving purchase orders from buyer agents
+		// Servidor de Pedidos de Compras
+		addBehaviour(new PurchaseOrdersServer());
 	}
 
-
 	/**
-	   Inner class RequestPerformer.
-	   This is the behaviour used by Book-buyer agents to request seller 
-	   agents the target book.
+	   Inner class OfferRequestsServer.
+	   This is the behaviour used by Book-seller agents to serve incoming requests 
+	   for offer from buyer agents.
+	   If the requested book is in the local catalogue the seller agent replies 
+	   with a PROPOSE message specifying the price. Otherwise a REFUSE message is
+	   sent back.
 	 */
-	private class RequestPerformer extends Behaviour {
+	// Servidor de Requisi��es de Ofertas
+	//FIPA PROTOCOLS: http://www.fipa.org/specs/fipa00030/
+	private class OfferRequestsServer extends CyclicBehaviour {
 
 		private static final long serialVersionUID = 1L;
-		private AID bestSeller; // The agent who provides the best offer 
-		private int bestPrice;  // The best offered price
-		private int repliesCnt = 0; // The counter of replies from seller agents
-		private MessageTemplate mt; // The template to receive replies
-		private int step = 0;
 
 		public void action() {
-			switch (step) {
-			case 0:
-				// Send the cfp to all sellers
-				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
-				for (int i = 0; i < sellerAgents.length; ++i) {
-					cfp.addReceiver(sellerAgents[i]);
-				} 
-				cfp.setContent(targetBookTitle);
-				cfp.setConversationId("book-trade");
-				cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
-				myAgent.send(cfp);
-				// Prepare the template to get proposals
-				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"),
-						MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
-				step = 1;
-				break;
-			case 1:
-				// Receive all proposals/refusals from seller agents
-				ACLMessage reply = myAgent.receive(mt);
-				if (reply != null) {
-					// Reply received
-					if (reply.getPerformative() == ACLMessage.PROPOSE) {
-						// This is an offer 
-						int price = Integer.parseInt(reply.getContent());
-						if (bestSeller == null || price < bestPrice) {
-							// This is the best offer at present
-							bestPrice = price;
-							bestSeller = reply.getSender();
-						}
-					}
-					repliesCnt++;
-					if (repliesCnt >= sellerAgents.length) {
-						// We received all replies
-						step = 2; 
-					}
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				// CFP Message received. Process it
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+
+				Integer price = (Integer) catalogue.get(title);
+				if (price != null) {
+					// The requested book is available for sale. Reply with the price
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(String.valueOf(price.intValue()));
 				}
 				else {
-					block();
+					// The requested book is NOT available for sale.
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("not-available");
 				}
-				break;
-			case 2:
-				// Send the purchase order to the seller that provided the best offer
-				ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
-				order.addReceiver(bestSeller);
-				order.setContent(targetBookTitle);
-				order.setConversationId("book-trade");
-				order.setReplyWith("order"+System.currentTimeMillis());
-				myAgent.send(order);
-				// Prepare the template to get the purchase order reply
-				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"),
-						MessageTemplate.MatchInReplyTo(order.getReplyWith()));
-				step = 3;
-				break;
-			case 3:      
-				// Receive the purchase order reply
-				reply = myAgent.receive(mt);
-				if (reply != null) {
-					// Purchase order reply received
-					if (reply.getPerformative() == ACLMessage.INFORM) {
-						// Purchase successful. We can terminate
-						System.out.println(targetBookTitle+" successfully purchased from agent "+reply.getSender().getName());
-						System.out.println("Price = "+bestPrice);
-						myAgent.doDelete();
-					}
-					else {
-						System.out.println("Attempt failed: requested book already sold.");
-					}
-
-					step = 4;
-				}
-				else {
-					block();
-				}
-				break;
-			}        
-		}
-
-		public boolean done() {
-			
-			if (step == 2 && bestSeller == null) {
-				System.out.println("Attempt failed: "+targetBookTitle+" not available for sale");
-			}
-			
-			boolean bookIsNotAvailable = (step == 2 && bestSeller == null);
-			boolean negotiationIsConcluded = (step == 4);
-			
-			boolean isDone = false;
-			if (bookIsNotAvailable || negotiationIsConcluded) {
-				isDone = true;
+				myAgent.send(reply);
 			}
 			else {
-				isDone = false;
+				block();
 			}
-			
-			return isDone;
-			//return ((step == 2 && bestSeller == null) || step == 4);
 		}
-	}  // End of inner class RequestPerformer
+	}  // End of inner class OfferRequestsServer
+
+	/**
+	   Inner class PurchaseOrdersServer.
+	   This is the behaviour used by Book-seller agents to serve incoming 
+	   offer acceptances (i.e. purchase orders) from buyer agents.
+	   The seller agent removes the purchased book from its catalogue 
+	   and replies with an INFORM message to notify the buyer that the
+	   purchase has been sucesfully completed.
+	 */
+	// Servidor de Pedidos de Compras
+	private class PurchaseOrdersServer extends CyclicBehaviour {
+
+		private static final long serialVersionUID = 1L;
+
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				// ACCEPT_PROPOSAL Message received. Process it
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+
+				Integer price = (Integer) catalogue.remove(title);
+				if (price != null) {
+					reply.setPerformative(ACLMessage.INFORM);
+					System.out.println(title+" sold to agent "+msg.getSender().getName());
+				}
+				else {
+					// The requested book has been sold to another buyer in the meanwhile .
+					reply.setPerformative(ACLMessage.FAILURE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
+	}  // End of inner class OfferRequestsServer
+	
+	
+	
+	/**
+    This is invoked by the GUI when the user adds a new book for sale
+	 */
+	public void updateCatalogue(final String title, final int price) {
+		addBehaviour(new OneShotBehaviour() {
+
+			private static final long serialVersionUID = 1L;
+
+			public void action() {
+				catalogue.put(title, new Integer(price));
+				System.out.println(title+" inserted into catalogue. Price = "+price);
+			}
+		} );
+	}
 	
 	
 	// Put agent clean-up operations here
-	protected void takeDown() {
-		// Printout a dismissal message
-		System.out.println("Buyer-agent "+getAID().getName()+" terminating.");
-	}
-	
+		protected void takeDown() {
+			// Deregister from the yellow pages
+			try {
+				DFService.deregister(this);
+			}
+			catch (FIPAException fe) {
+				fe.printStackTrace();
+			}
+			// Close the GUI
+			myGui.dispose();
+			// Printout a dismissal message
+			System.out.println("Seller-agent "+getAID().getName()+" terminating.");
+		}
 }
 
+
+
+}
