d1533f0b5ca28b9b8553ea58a2b3e1a77fb4314a
Lucas <l.lopes.fga@gmail.com>
2019-11-28 22:16:03 -0200

    Classe de teste
    Co-authored-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- CerradoPizzas/src/main/Main.java
+++ CerradoPizzas/src/main/Main.java
@@ -7,25 +7,6 @@ import util.Gerador;
 public class Main {
 
     public static void main(String[] args) {
-        final String prompt = "bash";
-        final String op = "-c";
-        String json;
-        String commmandSend;
-        final String url = "http://localhost:3000/post/";
-        ComunicadorAPI.enviarDados();
-//        ComunicadorAPI.enviarDados("echo '"+Gerador.novoPedidoJSON()+"'");
-        for(int i = 0; i < 10; i++)
-            try {
-                json = Gerador.novoPedidoJSON();
-                commmandSend = "curl --header \"Content-Type: application/json\" "
-                + "  --request POST "
-                + "  --data '" + json + "' ";
-                commmandSend += url;
-                ComunicadorAPI.enviarDados(prompt, op, commmandSend);
-                ComunicadorAPI.enviarDados("echo '"+Gerador.novoPedidoJSON()+"'");
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                e.printStackTrace();
-                }
+       ComunicadorAPI.enviarAPI(Gerador.novoPedidoJSON());
     }
 }
